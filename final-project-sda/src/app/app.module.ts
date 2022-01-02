d87532242680968cd4916fd25579dc7a6a017787
User <talisviirpalu@gmail.com>
2022-01-02 11:53:02 +0200

    generated pages with header

--- final-project-sda/src/app/app.module.ts
+++ final-project-sda/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './PAGES/home/home.component';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
+import { HomeMainComponent } from './COMPONENTS/home-main/home-main.component';
+import { AboutUsComponent } from './PAGES/about-us/about-us.component';
+import { ContactComponent } from './PAGES/contact/contact.component';
+import { WorkComponent } from './PAGES/work/work.component';
+import { FAQComponent } from './PAGES/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    HomeMainComponent,
+    AboutUsComponent,
+    ContactComponent,
+    WorkComponent,
+    FAQComponent
   ],
   imports: [
     BrowserModule,
