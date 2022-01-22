8290c529e3c24809225719ec2020126f54fc034f
User <talisviirpalu@gmail.com>
2022-01-22 22:36:11 +0200

    added data binded content to service mobile view

--- final-project-sda/src/app/PAGES/home/home.component.ts
+++ final-project-sda/src/app/PAGES/home/home.component.ts
@@ -6,8 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
+
+  webDevServ: any = [
+    "Website development",
+    "Corporate website",
+    "Startup websites",
+    "Landings and promo sites",
+    "E-commerce",
+    "Web applications development",
+    "Wordpress development"
+  ]
+  designServ: any = [
+    "Web design",
+    "Mobile app design",
+    "UX, UI design",
+    "Logo design",
+    "Brand Identity",
+    "Banner ad design"
+  ]
+  outsource: any = [
+    "Dedicated team",
+    "Hire Back-End Developer",
+    "Hire Front-End Developer",
+    "Hire Designer",
+    "Hire Project Manager"
+  ]
+  digiMark: any = [
+    "Conversion marketing",
+    "Content marketing",
+    "PPC services",
+    "Social media marketing services"
+  ]
+  seoServ: any = [
+    "Tech SEO",
+    "On-page SEO",
+    "Off-page SEO",
+    "SEO copywriting",
+  ]
+  busiPackage: any = [
+    "Fixed price website"
+  ]
 
   ngOnInit(): void {
   }
